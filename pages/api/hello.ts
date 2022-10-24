61ec3dd3e52e440d10faa9dff178e8f848c57019
Emil Privér <emil@priver.se>
2022-10-24 21:45:05 +0200

    init

---
+++ pages/api/hello.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+
+export default async function handler(req: NextRequest) {
+  return new Response(
+    JSON.stringify({
+      name: process.env.NEXT_RUNTIME,
+    }),
+    {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
+}
+
